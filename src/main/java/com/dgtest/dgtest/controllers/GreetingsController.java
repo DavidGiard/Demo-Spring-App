22a4620260eb5ce48925f871fd61bd65d9f30f9e
David Giard <davidjgiard@yahoo.com>
2022-08-26 12:32:52 -0500

    ResponseEnity, parameters

--- src/main/java/com/dgtest/dgtest/controllers/GreetingsController.java
+++ src/main/java/com/dgtest/dgtest/controllers/GreetingsController.java
@@ -1,15 +1,24 @@
 package com.dgtest.dgtest.controllers;
 
+import org.springframework.boot.autoconfigure.dao.PersistenceExceptionTranslationAutoConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("greetings")
 @RestController
 public class GreetingsController {
-
     @GetMapping("greet")
-    public String greet() {
-        return "Hello";
+    public ResponseEntity<String> greet() {
+        return new ResponseEntity<String>("Hello", HttpStatus.OK);
+    }
+
+    @GetMapping("greet/{personName}")
+    public ResponseEntity<String> greetPerson(@PathVariable("personName")String personName) {
+        String greeting = "Hello, " + personName;
+        return new ResponseEntity<String>(greeting, HttpStatus.OK);
     }
 }
